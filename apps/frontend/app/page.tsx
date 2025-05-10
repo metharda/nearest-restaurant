a29048b3c21a91b97ea38544c7b03914ef42cf2b
metharda <ahmetardaorak@gmail.com>
2025-05-10 17:03:45 +0300

    fixed page.tsx

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,10 +1,8 @@
 import Map from '@/components/map';
-import RestaurantSideMenu from "@/components/sidebar"
 export default function Home() {
   return (
     <main className="relative h-screen w-full">
       <Map />
-      <RestaurantSideMenu />
     </main>
   );
 }
